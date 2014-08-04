27e9efd650d91cd30783749b8b431aa767833add
Mario Hsieh <mariohsieh@gmail.com>
2014-08-04 02:18:51 -0700

    added angular, first couple pages

---
+++ js/app.js
@@ -0,0 +1,27 @@
+angular.module("introToBootstrapApp", ['ui.router'])
+
+	// config routes
+	.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+		$locationProvider.html5Mode(true);
+		
+		//$urlRouterProvider.otherwise('/');
+	
+		$stateProvider
+			.state('home', {
+				templateUrl: 'views/menu.html'
+			})
+			.state('home.welcome', {
+				url: '/',
+				templateUrl: 'views/00-welcome.html'
+			})			
+			.state('home.gettingStarted', {
+				url: 'getting-started',
+				templateUrl: 'views/01-getting-started.html'
+			});
+	})
+
+	.controller("indexCtrl", function($scope) {
+		console.log('hi');
+	});
+
