f2969ea39b8f2d8ca1ad8b8000abed2058f24b0f
Mario Hsieh <mariohsieh@gmail.com>
2014-08-04 05:06:33 -0700

    updated routing for uploading

--- js/app.js
+++ js/app.js
@@ -3,25 +3,30 @@ angular.module("introToBootstrapApp", ['ui.router'])
 	// config routes
 	.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 
-		$locationProvider.html5Mode(true);
+		//$locationProvider.html5Mode(true);
 		
 		//$urlRouterProvider.otherwise('/');
+		//$urlRouterProvider
+			//.when('','/home');
 	
 		$stateProvider
 			.state('home', {
+				//url: '/home/',
 				templateUrl: 'views/menu.html'
+				//template: '<h1>hello there</h1>'
 			})
+			
 			.state('home.welcome', {
-				url: '/',
 				templateUrl: 'views/00-welcome.html'
 			})			
 			.state('home.gettingStarted', {
 				url: 'getting-started',
 				templateUrl: 'views/01-getting-started.html'
 			});
+		
 	})
 
-	.controller("indexCtrl", function($scope) {
-		console.log('hi');
+	.controller("indexCtrl", function($scope, $state) {
+		$state.go('home.welcome');
 	});
 
